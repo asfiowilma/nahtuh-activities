4056aac63c1c129cfe0d9036d6d3f3fa49c3a587
asfiowilma <asfiolithaw@gmail.com>
2021-10-14 09:45:31 +0700

    [✨] added react boilerplate

---
+++ boilerplate/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <div class="mockup-window bg-base-300 w-96 mx-auto my-8">
+        <div class="flex justify-center px-4 py-16 bg-base-200">Hello world!</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
